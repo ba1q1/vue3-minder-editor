16ad1e507a4460725ef223e1c5c06a852d8f32a4
baiqi <qi.bai@fit2cloud.com>
2023-05-17 18:06:58 +0800

    feat: 国际化

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import mindEditor from './components/minderEditor.vue';
 import { setupI18n } from '@/locale';
-import store from '@/store';
 import '@7polo/kity/dist/kity.js';
 import 'hotbox-minder/hotbox.js';
 import '@7polo/kityminder-core';
@@ -10,6 +9,5 @@ export default {
   install: async (app: any) => {
     await setupI18n(app);
     app.component(mindEditor.name, mindEditor);
-    app.use(store);
   },
 };

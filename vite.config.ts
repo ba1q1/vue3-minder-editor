af94323bb2601ebcac5625dac4f12d39f3fa0f44
baiqi <qi.bai@fit2cloud.com>
2023-05-17 18:46:37 +0800

    chore: 发包配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,71 +10,73 @@ import IconsResolver from 'unplugin-icons/resolver';
 import replace from '@rollup/plugin-replace';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default ({ mode }: { mode: string }) =>
+  defineConfig({
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      // Auto import functions from Vue, e.g. ref, reactive, toRef...
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-      imports: ['vue'],
+    plugins: [
+      vue(),
+      AutoImport({
+        // Auto import functions from Vue, e.g. ref, reactive, toRef...
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ['vue'],
 
-      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
-      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-      resolvers: [
-        ElementPlusResolver(),
+        // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        resolvers: [
+          ElementPlusResolver(),
 
-        // Auto import icon components
-        // 自动导入图标组件
-        IconsResolver({
-          prefix: 'Icon',
-        }),
-      ],
-    }),
-    Components({
-      resolvers: [
-        // Auto register icon components
-        // 自动注册图标组件
-        IconsResolver({
-          enabledCollections: ['ep'],
-        }),
-        // Auto register Element Plus components
-        // 自动导入 Element Plus 组件
-        ElementPlusResolver(),
-      ],
-    }),
+          // Auto import icon components
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+      Components({
+        resolvers: [
+          // Auto register icon components
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep'],
+          }),
+          // Auto register Element Plus components
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+        ],
+      }),
 
-    Icons({
-      autoInstall: true,
-    }),
+      Icons({
+        autoInstall: true,
+      }),
 
-    replace({
-      preventAssignment: true,
-      // 将__VUE_I18N_FULL_INSTALL__替换为true
-      __VUE_I18N_FULL_INSTALL__: true,
-      // 将__VUE_I18N_LEGACY_API__替换为false
-      __VUE_I18N_LEGACY_API__: false,
-      // 将__VUE_I18N_PROD_DEVTOOLS__替换为false
-      __VUE_I18N_PROD_DEVTOOLS__: false,
-    }),
-    visualizer({ open: true, brotliSize: true, filename: 'report.html' }),
-  ],
-  optimizeDeps: {
-    include: ['vue', '@vueuse/core'],
-  },
-  build: {
-    lib: {
-      entry: 'src/main.ts',
-      name: 'vue3-minder-editor',
-      formats: ['es', 'umd', 'cjs'],
-      fileName: (format) => `vue3-minder-editor.${format}.js`,
+      replace({
+        preventAssignment: true,
+        // 将__VUE_I18N_FULL_INSTALL__替换为true
+        __VUE_I18N_FULL_INSTALL__: true,
+        // 将__VUE_I18N_LEGACY_API__替换为false
+        __VUE_I18N_LEGACY_API__: false,
+        // 将__VUE_I18N_PROD_DEVTOOLS__替换为false
+        __VUE_I18N_PROD_DEVTOOLS__: false,
+      }),
+      // 这里通过--mode analyze配置为分析模式，使用visualizer插件分析方法，输出report.html分析报告
+      process.env.NODE_ENV === 'analyze' ? visualizer({ open: true, brotliSize: true, filename: 'report.html' }) : null,
+    ],
+    optimizeDeps: {
+      include: ['vue', '@vueuse/core'],
     },
-    rollupOptions: {
-      external: ['vue', /^element-plus/],
+    build: {
+      lib: {
+        entry: 'src/main.ts',
+        name: 'vue3-minder-editor',
+        formats: ['es', 'umd', 'cjs'],
+        fileName: (format) => `vue3-minder-editor.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['vue', /^element-plus/],
+      },
     },
-  },
-});
+  });
